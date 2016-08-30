385df970d4fdd3cddeb5463d60f3f856109196e5
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-08-30 10:10:39 -0600

    init

---
+++ ddl/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE students (
+  id numeric,
+  first_name text,
+  last_name text,
+  jobs_applied text
+)
+
+CREATE TABLE teachers (
+  id numeric,
+  first_name text,
+  last_name text,
+)
+
+CREATE TABLE classes (
+  id numeric,
+  subject text
+)
